90a65cedd7fa75883e8d25bcf0617ce65c3b2e81
Kamil Medjidov <captain-kam@yandex.ru>
2026-03-20 20:05:36 +0400

    checkstyle

--- server/src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ server/src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -1,11 +1,8 @@
 package ru.practicum.shareit.item.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonSetter;
-import com.fasterxml.jackson.annotation.Nulls;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
 import lombok.*;
 import ru.practicum.shareit.booking.dto.BookingShortDto;
 
