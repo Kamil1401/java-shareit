c9bf62848d870cc54a1b32b3ad1ef029412d11f0
Kamil Medjidov <captain-kam@yandex.ru>
2026-03-20 20:08:58 +0400

    checkstyle

--- gateway/src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ gateway/src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -1,10 +1,7 @@
 package ru.practicum.shareit.item.dto;
 
-import com.fasterxml.jackson.annotation.JsonSetter;
-import com.fasterxml.jackson.annotation.Nulls;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
 import lombok.*;
 import ru.practicum.shareit.booking.dto.BookingShortDto;
 
--- gateway/src/main/java/ru/practicum/shareit/user/UserGatewayController.java
+++ gateway/src/main/java/ru/practicum/shareit/user/UserGatewayController.java
@@ -4,12 +4,9 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-
 import ru.practicum.shareit.user.dto.UserDto;
 
 @RestController
